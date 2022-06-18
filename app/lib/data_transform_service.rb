3f04bf3a8e42d961fc115a8f643198a062cd842c
Alexander <42774298+aposi324@users.noreply.github.com>
2022-06-17 22:31:24 -0400

    Complete challenge
    Complete transform method to write the parsed data to "out.json".
    
    Create unit tests to verify that the function (and helper functions) works correclty.

--- app/lib/data_transform_service.rb
+++ app/lib/data_transform_service.rb
@@ -1,7 +1,7 @@
 require 'csv'
 
 class DataTransformService
-
+  
   # ==== This method does:
   #
   #  Iterates over the csv and transforms it into JSON documents
@@ -21,15 +21,99 @@ class DataTransformService
   def transform(file_path)
     Rails.logger.info("Processing file from location: [#{file_path}]")
     row_counter = 0
-
+    parsed_data = []
+    
     CSV.open(file_path, 'r:UTF-8', headers: true) do |csv|
       csv.each do |csv_row|
-        Rails.logger.debug("Processing row: #{csv_row}")
+        row_data = {}
+        images = {}
+        attributes = {}
 
+
+        csv_row.each_with_index do |csv_cell, column|
+          if is_in_image_range(column) then
+            images[csv_cell[0]] = csv_cell[1]
+          elsif is_in_attributes_range(column) then
+            attributes[csv_cell[0]] = csv_cell[1]
+          else
+            row_data[csv_cell[0]] = csv_cell[1]
+          end
+        end
+
+        row_data["images"] = images
+        row_data["attributes"] = attributes
+        parsed_data[row_counter] = row_data
         row_counter += 1
       end
     end
-    row_counter
+
+    write_to_file(parsed_data)
+    return row_counter
+  end
+
+  # ==== This method does:
+  #
+  #  Writes an object to a file, serialized as JSON
+  #
+  # ==== With params:
+  #
+  # @param object <object> - Object to be written to file as json
+  # @param path <String> - Output file path. Default value: "out.json"
+  #
+  # ==== And returns:
+  #
+  # @return void
+  #
+  # ==== And is used like:
+  #
+  # write_to_file(object) 
+  # write_to_file(object, "path")
+  #
+  def write_to_file(object, path = "out.json")
+    json = JSON.pretty_generate(object)
+    out_file = File.new(path, "w")
+    out_file.puts(json)
+    out_file.close
+  end
+
+  # ==== This method does:
+  #
+  #  Given a column ordinal, this function returns whether or not it belongs in the images subcategory
+  #
+  # ==== With params:
+  #
+  # @param column_ordinal_index <integer> - Ordinal index that is checked
+  #
+  # ==== And returns:
+  #
+  # @return True if the index belongs in the images subcategory, false if it doesn't
+  #
+  # ==== And is used like:
+  #
+  # is_in_image_range(column_rodinal)
+  #
+  def is_in_image_range(column_ordinal_index)
+    return (8..11) === column_ordinal_index
+  end
+
+  # ==== This method does:
+  #
+  #  Given a column ordinal, this function returns whether or not it belongs in the attributes subcategory
+  #
+  # ==== With params:
+  #
+  # @param column_ordinal_index <integer> - Ordinal index that is checked
+  #
+  # ==== And returns:
+  #
+  # @return True if the index belongs in the attributes subcategory, false if it doesn't
+  #
+  # ==== And is used like:
+  #
+  # is_in_attributes_range(column_rodinal)
+  #
+  def is_in_attributes_range(column_ordinal_index)
+    return (12..31) === column_ordinal_index
   end
 
 end
--- spec/lib/data_transform_service_spec.rb
+++ spec/lib/data_transform_service_spec.rb
@@ -3,16 +3,122 @@ require 'rails_helper'
 RSpec.describe DataTransformService do
 
   let!(:csv_path) { 'spec/csv/sample-part-data.csv'}
+  let!(:json_path) { 'out.json'}
   let!(:service) { DataTransformService.new}
-
+  
   describe '#transform' do
+  
+    it 'Should return number of processed rows' do
+      result = service.transform(csv_path)
+      expect(result).to eql(25)
+    end
 
     it 'should transform rows in the passed file from csv to JSON docs' do
       result = service.transform(csv_path)
-      expect(result).to eql(25)
+      file = File.open(json_path)
+      file_data = file.read
+      object = JSON.parse(file_data)
+      expect(object.length()).to eql(25)
+      file.close()
+    end
+
+
+    it 'should parse first row' do
+      result = service.transform(csv_path)
+      file = File.open(json_path)
+      file_data = file.read
+      file.close()
+      object = JSON.parse(file_data)
+      first_row = object[0]
+      expect(first_row["sku"]).to eql("10061122131120HLF")
 
     end
 
+    it 'should parse last row' do
+      result = service.transform(csv_path)
+      file = File.open(json_path)
+      file_data = file.read
+      file.close()
+      object = JSON.parse(file_data)
+      last_row = object[object.length-1]
+      expect(last_row["sku"]).to eql("10064555332110ELF")
+    end
+
+    it 'should parse images sub-category' do
+      result = service.transform(csv_path)
+      file = File.open(json_path)
+      file_data = file.read
+      file.close()
+      object = JSON.parse(file_data)
+      first_row = object[0]
+      expect(first_row["images"]["image"]).to eql("http://www.amphenol-icc.com/media/wysiwyg/files/pn-image/yll_d 10061122.jpg")
+      expect(first_row["images"]["3d_model_iges"]).to eql("https://cdn.amphenol-icc.com/media/wysiwyg/files/3d/i10061122-131120hlf.zip")
+
+    end
+
+    it 'should parse attributes sub-category' do 
+      result = service.transform(csv_path)
+      file = File.open(json_path)
+      file_data = file.read
+      file.close()
+      object = JSON.parse(file_data)
+      first_row = object[0]
+      last_row = object[object.length-1]
+      expect(first_row["attributes"]["eu_rohs_y"]).to eql("Yes")
+      expect(first_row["attributes"]["voltage_rating"]).to eql("30V")
+      expect(last_row["termination_style_filter"]).to eql(nil)
+    end
   end
 
+
+  describe '#is_in_image_range' do
+    it 'Returns true for columns that describe the images sub-category' do
+      expect(service.is_in_image_range(8)).to eql(true)
+      expect(service.is_in_image_range(9)).to eql(true)
+      expect(service.is_in_image_range(10)).to eql(true)
+      expect(service.is_in_image_range(11)).to eql(true)
+    end
+
+    it 'Returns false for columns that describe the images sub-category' do
+      expect(service.is_in_image_range(-1)).to eql(false)
+      expect(service.is_in_image_range(0)).to eql(false)
+      expect(service.is_in_image_range(7)).to eql(false)
+      expect(service.is_in_image_range(12)).to eql(false)
+    end
+  end
+
+
+  describe '#is_in_attributes_range' do
+    it 'Returns true for columns that describe the attributes sub-category' do
+      expect(service.is_in_attributes_range(12)).to eql(true)
+      expect(service.is_in_attributes_range(20)).to eql(true)
+      expect(service.is_in_attributes_range(30)).to eql(true)
+      expect(service.is_in_attributes_range(31)).to eql(true)
+    end
+
+    it 'Returns false for columns that describe the attributes sub-category' do
+      expect(service.is_in_attributes_range(-1)).to eql(false)
+      expect(service.is_in_attributes_range(8)).to eql(false)
+      expect(service.is_in_attributes_range(11)).to eql(false)
+      expect(service.is_in_attributes_range(32)).to eql(false)
+    end
+  end
+  
+
+  describe '#write_to_file' do
+    it 'Writes object as valid JSON' do
+      object = { }
+      object["key"] = "value"
+      service.write_to_file(object, "test.json")
+      file = File.open("test.json")
+      file_data = file.read()
+      file.close()
+
+      parsed_object = JSON.parse(file_data)
+
+      expect(parsed_object["key"]).to eql(object["key"])
+
+      File.delete("test.json")
+    end
+  end
 end
